59a6095d0b3612883ca7a26282768d3b31f1d414
rashidjb <rjburesly@gmail.com>
2017-07-23 18:55:04 +0300

    created an ignore file

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,14 @@
+from __future__ import unicode_literals
 from django.contrib import admin
+from .models import Post
+
 
 # Register your models here.
+class PostModelAdmin (admin.ModelAdmin):
+	list_display = ["title", "timestamp", "updated"]
+	search_fields = ["title", "content"]
+	class Meta:
+		model = Post
+
+
+admin.site.register(Post)
