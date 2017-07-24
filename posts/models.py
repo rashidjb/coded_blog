fb43720afdd26d3fe4f9c687bc20b835bf27eae7
rashidjb <rjburesly@gmail.com>
2017-07-24 19:43:44 +0300

    added url method

--- posts/models.py
+++ posts/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
-    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+	title = models.CharField(max_length=255)
+	content = models.TextField()
+	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
-    def __str__(self):
-        return self.title
+	def __str__(self):
+		return self.title
+
+	def get_abs_url(self):
+		return reverse("posts:detail", kwargs={"post_id": self.id})
