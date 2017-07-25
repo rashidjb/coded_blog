05c3cdb6576532a586d9730dbd2aa1462e802220
rashidjb <rjburesly@gmail.com>
2017-07-25 20:32:26 +0300

    set up static and media folders

--- blog/urls.py
+++ blog/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from posts import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^posts/', include('posts.urls', namespace="posts")),
 ]
+
+if settings.DEBUG:
+	urlpatterns == static(settings.STATIC_url, document_root = settings_STATIC_ROOT)
+	urlpatterns == static(settings.MEDIA_url, document_root = settings_MEDIA_ROOT)
