c5729e5a0af4d46dfa4432e341f85301e085f251
rashidjb <rjburesly@gmail.com>
2017-07-26 21:02:15 +0300

    Pretty pictures

--- posts/views.py
+++ posts/views.py
@@ -3,12 +3,13 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from .forms import PostForm
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_home(request):
 	return HttpResponse("<h1>Hello</h1>")
 
 def post_create(request):
-	form = PostForm(request.POST or None) 
+	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
 		form.save()
 		messages.success(request, "You did it!... eventually.")
@@ -21,25 +22,37 @@ def post_create(request):
 	return render(request, 'post_create.html', context)
 
 def post_detail(request, post_id):
-    instance = get_object_or_404(Post, id=post_id)
-    context = {
-    "title": "Detail",
-    "instance": instance
-    }
-    return render(request, 'post_detail.html', context)
+	instance = get_object_or_404(Post, id=post_id)
+	context = {
+	"title": "Detail",
+	"instance": instance
+	}
+	return render(request, 'post_detail.html', context)
 	
 def post_list(request):
-    object_list = Post.objects.all()#.order_by("-updated", "-title")
-    context = {
-    "object_list": object_list,
-    "title": "List",
-    "user": request.user
-    }
-    return render(request, 'post_list.html', context)
+	object_list = Post.objects.all()
+	paginator = Paginator(object_list, 4) # Show 25 contacts per page
+
+	page = request.GET.get('page')
+	try:
+		objects = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		objects = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		objects = paginator.page(paginator.num_pages)
+
+	context = {
+	'objects': objects,
+	"title": "List",
+	"user": request.user
+	}
+	return render(request, 'post_list.html', context)
 
 def post_update(request, post_id):
 	post_object = get_object_or_404(Post, id=post_id)
-	form = PostForm(request.POST or None, instance = post_object) 
+	form = PostForm(request.POST or None, request.FILES or None, instance = post_object) 
 	if form.is_valid():
 		form.save()
 		return redirect("posts:list")
